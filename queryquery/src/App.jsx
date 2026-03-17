293a2ad1ea5f5614bd337b8d3a234d6c166c1013
Anant <anantpathak564@gmail.com>
2026-03-17 20:34:28 +0530

    got profiles from queries

--- queryquery/src/App.jsx
+++ queryquery/src/App.jsx
@@ -1,15 +1,43 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(null)
+  const [page, setPage ] = useState(1)
 
-  return (
+  const dataFetching = async()=>{
+    console.log(page)
+    const res = await fetch(`https://api.github.com/search/users?q=react&page=${page}&per_page=20`)
+    const newData = await res.json()
+    setData((data)=> data? data + newData : newData)
+    console.log(data)
+  }
+
+  useEffect(()=>{
+    dataFetching()
+  },[page])
+
+  return data? (
     <>
-    <div className='w-[400px] h-[40px] border bg-red-600'>This query query practice</div>
+    <div>
+      <div className='flex flex-wrap gap-5 m-5'>      {
+        data.items?.map((item)=>
+        <div key={item.id}>
+          <img src={`${item.avatar_url}`} alt="" className='w-[50px] h-[50px]' />
+           </div>
+
+        
+        )
+      }
+         </div>
+         <button className='btn bg-slate-500 p-3 rounded-md' onClick={()=> setPage((page)=> page+1 )} > more...</button>
+
+    </div>
     </>
+  ) : (
+    <div> Loading...</div>
   )
 }
 
