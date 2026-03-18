b99b0b564d6b123c5759f8514a5e074b29d4c68c
Anant <anantpathak564@gmail.com>
2026-03-18 20:29:38 +0530

    Pagination and Search functionality applied

--- queryquery/src/App.jsx
+++ queryquery/src/App.jsx
@@ -5,46 +5,65 @@ import heroImg from './assets/hero.png'
 
 function App() {
   const [data, setData] = useState(null)
-  const [page, setPage ] = useState(1)
+  const [page, setPage] = useState(2)
 
   const [person, setPerson] = useState("")
 
-  const dataFetching = async()=>{
+  const dataFetching = async () => {
     console.log(page)
-    const res = await fetch(`https://api.github.com/search/users?q=react&page=${page}&per_page=20`)
+    const res = await fetch(`https://api.github.com/search/users?q=${person}&page=1&per_page=20`)
     const newData = await res.json()
     const items = newData.items
-    // console.log(newData)
-    // setData((data)=> data? [...data, ...items]: items)
+    console.log(newData)
+    setData(items)
 
   }
-    
+  const similarDataFetching = async () => {
 
-  useEffect(()=>{
-    dataFetching()
-  },[page])
+    if (page > 1) {
+      console.log("Hii im here")
+      const res = await fetch(`https://api.github.com/search/users?q=${person}&page=${page}&per_page=20`)
+      const data = await res.json()
+      const items = data.items
+      setData((data) => data ? [...data, ...items] : items)
+    } 
+  }
 
- 
-  return data? (
+
+
+  
+
+
+  return data ? (
     <>
-    <div>
-     
-      <div className='flex flex-wrap gap-5 m-5'>      {
-        data.map((item)=>
-        <div key={item.id}>
-          <img src={`${item.avatar_url}`} alt="" className='w-[50px] h-[50px]' />
-           </div>
+      <div>
 
-        
-        )
-      }
-         </div>
-         <button className='btn bg-slate-500 p-3 rounded-md' onClick={()=> setPage((page)=> page+1 )} > more...</button>
+        <div>
+          <input type="text" id='person' className='input w-[180px] h-[40px] border border-black rounded-lg ml-[600px] p-2' value={person} onChange={(e) => setPerson(e.target.value)} />
+          <button type='submit' onClick={() => dataFetching()} className='w-[150px] h-[40px] rounded-lg'> Search</button>
+        </div>
 
-    </div>
+        <div className='flex flex-wrap gap-5 m-5'>      {
+          data.map((item) =>
+            <div key={item.id}>
+              <img src={`${item.avatar_url}`} alt="" className='w-[50px] h-[50px]' />
+              <div className='text-black text-md'>{item.login}</div>
+            </div>
+
+
+          )
+        }
+        </div>
+        <button className='btn bg-slate-500 p-3 rounded-md' onClick={() => { setPage((page) => page + 1), similarDataFetching() }} > more...</button>
+
+      </div>
     </>
   ) : (
-    <div>  <input type="text" className='input w-[180px] h-[40px] border border-black rounded-lg ml-[600px]' value={person} /></div>
+    <div>
+      <input type="text" id='person' className='input w-[180px] h-[40px] border border-black rounded-lg ml-[600px] p-2' value={person} onChange={(e) => setPerson(e.target.value)} />
+      <button type='submit' onClick={() => dataFetching()} className='w-[150px] h-[40px] rounded-lg'> Search</button>
+
+    </div>
   )
 }
 
